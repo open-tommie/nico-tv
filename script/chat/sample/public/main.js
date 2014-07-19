86159b4fcad48cef91a82f2537e2b66b90f5ee9a
open-tommie <open.tommie@gmail.com>
2014-07-19 22:03:29 +0900

    サーバーの命令でビデオ開始

--- script/chat/sample/public/main.js
+++ script/chat/sample/public/main.js
@@ -1,163 +1,218 @@
+//
+//	noco-tv client main
+//	by @tommie_nico
+//	this code is based on XXXX
+//
+
 $(function() {
-  console.log( "main start");
-  var g_message = "";
-  var g_maxMessage = 100;
-  
-  function addMsg( msg) {
-  	// xxx g_messageを配列にせよ
-	g_message += "\n"+msg;
-	var lines = g_message.split( "\n");
-	if ( lines.length > g_maxMessage) {
-		lines.splice( 0, lines.length - g_maxMessage);
+	function log( str) {
+		console.log( str);
 	}
-	g_message = lines.join("\n");
+	log("main start");
+	var g_message = "";
+	var g_maxMessage = 100;
+	// Initialize varibles
+	var $window = $(window);
+	var $usernameInput = $('.usernameInput'); // Input for username
+	var $messages = $('.messages'); // Messages area
+	var $inputMessage = $('#inputMessage'); // Input message input box
+	// Prompt for setting a username
+	var username;
+	var connected = false;
+	var $currentInput = $usernameInput.focus();
+	var g_socket = io();
+
+	function init() {
+		log( "init() start");
 	
-	var ta = $("#taMessage");
-	ta.val( g_message);
-	ta.scrollTop(
-		ta[0].scrollHeight - ta.height()
-	);
+		function computeDuration(ms){
+			var h = String(Math.floor(ms / 3600000) + 100).substring(1);
+			var m = String(Math.floor((ms - h * 3600000)/60000)+ 100).substring(1);
+			var s = String(Math.round((ms - h * 3600000 - m * 60000)/1000)+ 100).substring(1);
+			return h+':'+m+':'+s;
+		}   
 
-  }
-  addMsg( "サーバーへ接続中…");
-  
-  // Initialize varibles
-  var $window = $(window);
-  var $usernameInput = $('.usernameInput'); // Input for username
-  var $messages = $('.messages'); // Messages area
-  var $inputMessage = $('#inputMessage'); // Input message input box
+		var video = document.getElementById("Video1");                                               
+		if (!video.canPlayType) {
+			log( "再生できない形式");
+			return;
+		}
+	 	video.pause();
+	
+		video.src = "http://tv.tommie2.info/co1827022/watch-1404825242.mp4";
+	/*****
+		video.src = "watch-1404825242.webm";
+		if (video.canPlayType) {
+			// CanPlayType returns maybe, probably, or an empty string.
+			//var playMsg = video.canPlayType('video/mp4; codecs="avc1.42E01E"');
+			var playMsg = video.canPlayType('video/mp4');
+			if ("" != playMsg) {
+				log( "mp4/H.264 is " + playMsg + " supported ");
+				video.src = "watch-1404825242.mp4";
+			}
+			playMsg = video.canPlayType('video/ogg; codecs="theora"');
+			if ("" != playMsg) {
+				log( "ogg is " + playMsg + " supported");
+			}
+			playMsg = video.canPlayType('video/webm; codecs="vp8, vorbis"');
+			if ("" != playMsg) {
+				log( "webm is " + playMsg + " supported");
+			}
+		}else {
+				log( "no video support");
+		}
+	*********/
+		setInterval( function() {
+			var ct = video.currentTime;
+			//log( "current="+ct);
+			//log( "computeDuration="+computeDuration(ct));
+			$("#currentTime").text( computeDuration(ct*1000));			
+		},100);
 
-  var $loginPage = $('.login.page'); // The login page
-  var $chatPage = $('.chat.page'); // The chatroom page
+		g_socket.emit('add user', "user1");
+		$currentInput.focus();
 
-  // Prompt for setting a username
-  var username;
-  var connected = false;
-  var typing = false;
-  var lastTypingTime;
-  var $currentInput = $usernameInput.focus();
+		log( "init() end");
+	}
 
+	function addMsg(msg) {
+		// xxx g_messageを配列にせよ
+		g_message += "\n" + msg;
+		var lines = g_message.split("\n");
+		if (lines.length > g_maxMessage) {
+			lines.splice(0, lines.length - g_maxMessage);
+		}
+		g_message = lines.join("\n");
 
-  var socket = io();
+		var ta = $("#taMessage");
+		ta.val(g_message);
+		ta.scrollTop(ta[0].scrollHeight - ta.height());
 
-  // Sets the client's username
-  function setUsername () {
-/***********************************del
-    username = cleanInput($usernameInput.val().trim());
-
-    // If the username is valid
-    if (username) {
-      $loginPage.fadeOut();
-      $chatPage.show();
-      $loginPage.off('click');
-      $currentInput = $inputMessage.focus();
-
-      // Tell the server your username
-      socket.emit('add user', username);
-    }
-*************************************/
-      username = "nobody";
-      socket.emit('add user', username);
-  }
+	}
+	addMsg("サーバーへ接続中…");
 
 
-  // Sends a chat message
-  function sendMessage () {
-    var message = $inputMessage.val();
-    // Prevent markup from being injected into the message
-    message = cleanInput(message);
-    // if there is a non-empty message and a socket connection
-    if (message && connected) {
-      $inputMessage.val('');
-      // tell server to execute 'new message' and send along one parameter
-      socket.emit('new message', message);
-    }
-  }
+	// Sets the client's username
+	function setUsername() {
+		username = "nobody";
+		g_socket.emit('add user', username);
+	}
 
-  // Log a message
-  function log (message, options) {
-/*******************d
-    var $el = $('<li>').addClass('log').text(message);
-    addMessageElement($el, options);
-*********************/
-  }
+	// Sends a chat message
+	function sendMessage() {
+		var message = $inputMessage.val();
+		// Prevent markup from being injected into the message
+		message = cleanInput(message);
+		// if there is a non-empty message and a socket connection
+		if (message && connected) {
+			$inputMessage.val('');
+			// tell server to execute 'new message' and send along one parameter
+			g_socket.emit('new message', message);
+		}
+	}
 
-  // Adds the visual chat message to the message list
-  function addChatMessage (data, options) {
-	addMsg( data.time + " " + data.message);
+	// Adds the visual chat message to the message list
+	function addChatMessage(data, options) {
+		addMsg(data.time + " " + data.message);
 
-  }
+	}
 
-  // Prevents input from having injected markup
-  function cleanInput (input) {
-    return $('<div/>').text(input).text();
-  }
+	// Prevents input from having injected markup
+	function cleanInput(input) {
+		return $('<div/>').text(input).text();
+	}
 
-  // Keyboard events
+	// Keyboard events
 
-  $window.keydown(function (event) {
-    // Auto-focus the current input when a key is typed
-    if (!(event.ctrlKey || event.metaKey || event.altKey)) {
-      $currentInput.focus();
-    }
-    // When the client hits ENTER on their keyboard
-    if (event.which === 13) {
-      sendMessage();
-    }
-  });
+	$window.keydown(function(event) {
+		// Auto-focus the current input when a key is typed
+		if (!(event.ctrlKey || event.metaKey || event.altKey)) {
+			$currentInput.focus();
+		}
+		// When the client hits ENTER on their keyboard
+		if (event.which === 13) {
+			sendMessage();
+		}
+	});
 
-  $inputMessage.on('input', function() {
-    
-  });
+	$inputMessage.on('input', function() {});
 
-  // Focus input when clicking on the message input's border
-  $inputMessage.click(function () {
-    $inputMessage.focus();
-  });
+	// Focus input when clicking on the message input's border
+	$inputMessage.click(function() {
+		$inputMessage.focus();
+	});
 
-  // Socket events
+	//--------------------------------------------------------------------------
+	// Socket events
+	//--------------------------------------------------------------------------
 
-  socket.on('connect', function (data) {
-    console.log( "onConnect");
-  });
-  socket.on('login', function (data) {
-    console.log( "onLogin totalUser="+data.numUsers);
-    connected = true;
-    // Display the welcome message
-    var message = "ようこそ、暗黒TVへ！";
-	addMsg( "接続しました！");
-	addChatMessage( data);
-    
-    $("#totalUser").text( data.numUsers);
+	g_socket.on('connect', function(data) {
+		log("onConnect");
+	});
+	g_socket.on('login', function(data) {
+		log("onLogin totalUser=" + data.numUsers);
+		connected = true;
+		// Display the welcome message
+		var message = "ようこそ、暗黒TVへ！";
+		addMsg("接続しました！");
+		addChatMessage(data);
 
-  });
+		$("#totalUser").text(data.numUsers);
 
-  // Whenever the server emits 'new message', update the chat body
-  socket.on('new message', function (data) {
-  	try {
-  		console.log( "new message");
-		//console.log( "time=" + data.time);
-    	addChatMessage(data);
-    }
-    catch (e) {
-    	console.log( "ERROR e="+e);
-    }
-  });
+	});
+	g_socket.on('play', function(data) {
+		try {
+			log("onPlay"+", url="+data.url+", time="+data.time);
+			var video = document.getElementById("Video1");                                               
+			video.src = "http://tv.tommie2.info/co1827022/watch-1404825242.mp4";
+			video.addEventListener("error", function(err) {
+				log( err);
+				log( err.stack);
+			}, true);
+			video.addEventListener("loadstart", function(err) {
+				log( "on loadstart, video.src="+video.src);
+			}, true);
+			video.load(); 
+			video.addEventListener("loadedmetadata", function(err) {
+				log( "on loadedmetadata, video.src="+video.src);
+				log( "video.src="+video.src);
+				log( "video.duration="+video.duration);
+				video.currentTime = data.time;
+				video.play();
+			}, true);
+		}
+		catch( e) {
+			log( "ERROR "+e);
+			log( e.stack);
+		}
+	});
 
-  // Whenever the server emits 'user joined', log it in the chat body
-  socket.on('user joined', function (data) {
-    console.log(data.username + ' joined');
-    $("#totalUser").text( data.numUsers);
-  });
+	// Whenever the server emits 'new message', update the chat body
+	g_socket.on('new message', function(data) {
+		try {
+			log("new message");
+			//log( "time=" + data.time);
+			addChatMessage(data);
+			
+		}
+		catch (e) {
+			log("ERROR e=" + e);
+		}
+	});
 
-  // Whenever the server emits 'user left', log it in the chat body
-  socket.on('user left', function (data) {
-    console.log(data.username + ' left');
-    $("#totalUser").text( data.numUsers);
-  });
+	// Whenever the server emits 'user joined', log it in the chat body
+	g_socket.on('user joined', function(data) {
+		log(data.username + ' joined');
+		$("#totalUser").text(data.numUsers);
+	});
 
-  socket.emit('add user', "user1");
-  $currentInput.focus();
+	// Whenever the server emits 'user left', log it in the chat body
+	g_socket.on('user left', function(data) {
+		log(data.username + ' left');
+		$("#totalUser").text(data.numUsers);
+	});
 
-  console.log( "main end");
+
+	init();
+	log("main end");
 });
